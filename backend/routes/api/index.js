8dd65334bba53d93bc2271479159a9f7217edb08
root <root@Vincent-GalaxyBook2Pro>
2024-04-06 14:03:14 -0700

    add user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 
 router.use(restoreUser);
 
-
+router.use('/session', sessionRouter)
+router.use('/users', usersRouter)
 
 module.exports = router;
