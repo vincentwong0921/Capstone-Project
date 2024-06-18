006d43f0b06200879a8d112e12833f1a8d2ec788
root <vincentwong0921@gmail.com>
2024-06-17 23:29:52 -0700

    testing

--- frontend/src/components/Order/DeleteOrderModal.jsx
+++ frontend/src/components/Order/DeleteOrderModal.jsx
@@ -1,41 +1,40 @@
-import './DeleteOrderModal.css'
+import "./DeleteOrderModal.css";
 import { useModal } from "../../context/Modal";
 import { useDispatch } from "react-redux";
-import { removeOrder } from '../../store/order';
+import { removeOrder } from "../../store/order";
 
 function DeleteOrderModal({ setSelectedStatus, order }) {
-    const dispatch = useDispatch();
-    const { closeModal } = useModal();
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
-    const confirmDelete = async (e) => {
-      e.preventDefault();
-      await dispatch(removeOrder(order.id));
-      setSelectedStatus('All Orders')
-      closeModal();
-    };
+  const confirmDelete = async (e) => {
+    e.preventDefault();
+    await dispatch(removeOrder(order.id));
+    setSelectedStatus("All Orders");
+    closeModal();
+  };
 
-    const nope = (e) => {
-      e.preventDefault();
-      closeModal();
-    };
+  const nope = (e) => {
+    e.preventDefault();
+    closeModal();
+  };
 
-    return (
-        <form className="DeleteOrderForm">
-          <h2>Confirm Cancel</h2>
-          <div className="DeleteMsg">
-            Are you sure you want to cancel this order?
-          </div>
-          <div className="DeleteOrderButtons">
-            <button className="DeleteButton" onClick={confirmDelete}>
-              Yes (Delete Order)
-            </button>
-            <button className="NoDeleteButton" onClick={nope}>
-              No (Keep Order)
-            </button>
-          </div>
-        </form>
-      );
+  return (
+    <form className="DeleteOrderForm">
+      <h2>Confirm Cancel</h2>
+      <div className="DeleteMsg">
+        Are you sure you want to cancel this order?
+      </div>
+      <div className="DeleteOrderButtons">
+        <button className="DeleteButton" onClick={confirmDelete}>
+          Yes (Delete Order)
+        </button>
+        <button className="NoDeleteButton" onClick={nope}>
+          No (Keep Order)
+        </button>
+      </div>
+    </form>
+  );
 }
 
-
-export default DeleteOrderModal
+export default DeleteOrderModal;
